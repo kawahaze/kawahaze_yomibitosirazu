beff61dfd4a85b52117dd886550e97aa29fdbb10
kawahaze <kawahaze2023@gmail.com>
2026-07-23 19:50:41 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+let current = 0;
+
+const poem = document.getElementById("poem");
+const number = document.getElementById("number");
+
+function show(){
+
+poem.innerHTML = poems[current].replace(/\n/g,"<br>");
+
+number.textContent =
+(current+1) + " / " + poems.length;
+
+}
+
+show();
+
+document.getElementById("next").onclick=()=>{
+
+current++;
+
+if(current>=poems.length){
+
+current=0;
+
+}
+
+show();
+
+}
+
+document.getElementById("prev").onclick=()=>{
+
+current--;
+
+if(current<0){
+
+current=poems.length-1;
+
+}
+
+show();
+
+}
